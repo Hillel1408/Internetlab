baad2df22efc0029fa75eab6601d9cb721ce8181
Hillel1408 <etemax@bk.ru>
2024-07-01 15:44:09 +0300

    text block

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Layout, Intro, HowItWorks } from 'components'
+import { Layout, Intro, HowItWorks, TextBlock } from 'components'
 
 function App() {
   return (
@@ -6,6 +6,7 @@ function App() {
       <>
         <Intro />
         <HowItWorks />
+        <TextBlock />
       </>
     </Layout>
   )
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,5 +4,6 @@ import Footer from 'components/Footer/Footer'
 import Intro from 'components/Intro/Intro'
 import Button from 'components/Button/Button'
 import HowItWorks from 'components/HowItWorks/HowItWorks'
+import TextBlock from 'components/TextBlock/TextBlock'
 
-export { Layout, Header, Footer, Intro, Button, HowItWorks }
+export { Layout, Header, Footer, Intro, Button, HowItWorks, TextBlock }
