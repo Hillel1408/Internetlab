8ef14165c6cf8fec06c88ea74c713a95b959d278
Hillel1408 <etemax@bk.ru>
2024-07-01 14:01:59 +0300

    Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,12 @@
+import styles from 'components/Footer/Footer.module.scss'
+import globalStyles from 'styles/Globals.module.scss'
+
 export default function Footer() {
-  return <footer></footer>
+  return (
+    <footer className={styles.root}>
+      <div className={globalStyles.container}>
+        <span className={globalStyles.text18}>© 2021 Лаборатория интернет</span>
+      </div>
+    </footer>
+  )
 }
